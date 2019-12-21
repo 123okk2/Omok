e8b3242dd23b7d368b6df349570d6f9cf5f6fdc1
MinWoo LEE <123okk2@naver.com>
2019-12-21 19:55:43 +0900

    SAVE

---
+++ Omok/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Omok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.메뉴ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.시작ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.중지ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.재개ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 540);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.메뉴ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(561, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 메뉴ToolStripMenuItem
+            // 
+            this.메뉴ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.시작ToolStripMenuItem,
+            this.재개ToolStripMenuItem,
+            this.중지ToolStripMenuItem,
+            this.종료ToolStripMenuItem});
+            this.메뉴ToolStripMenuItem.Name = "메뉴ToolStripMenuItem";
+            this.메뉴ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.메뉴ToolStripMenuItem.Text = "메뉴";
+            // 
+            // 시작ToolStripMenuItem
+            // 
+            this.시작ToolStripMenuItem.Name = "시작ToolStripMenuItem";
+            this.시작ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.시작ToolStripMenuItem.Text = "새로시작";
+            this.시작ToolStripMenuItem.Click += new System.EventHandler(this.시작ToolStripMenuItem_Click);
+            // 
+            // 중지ToolStripMenuItem
+            // 
+            this.중지ToolStripMenuItem.Name = "중지ToolStripMenuItem";
+            this.중지ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.중지ToolStripMenuItem.Text = "중지";
+            this.중지ToolStripMenuItem.Click += new System.EventHandler(this.중지ToolStripMenuItem_Click);
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
+            this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
+            // 
+            // 재개ToolStripMenuItem
+            // 
+            this.재개ToolStripMenuItem.Name = "재개ToolStripMenuItem";
+            this.재개ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.재개ToolStripMenuItem.Text = "재개";
+            this.재개ToolStripMenuItem.Click += new System.EventHandler(this.재개ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 583);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 메뉴ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 시작ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 중지ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 재개ToolStripMenuItem;
+    }
+}
+
---
+++ Omok/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Omok
+{
+    public partial class Form1 : Form
+    {
+        int[,] pan = new int[19,19];
+        int order = 0; // 검은 돌
+        SolidBrush black = new SolidBrush(Color.Black);
+        SolidBrush white = new SolidBrush(Color.White);
+        int hw, vw;
+
+        Boolean isGame = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            중지ToolStripMenuItem.Enabled = false;
+            재개ToolStripMenuItem.Enabled = false;
+
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 19; j++) pan[i, j] = 2;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            hw = pictureBox1.Width / 20;
+            vw = pictureBox1.Height / 20;
+            Pen p = new Pen(Color.Black);
+            SolidBrush b = new SolidBrush(Color.Black);
+
+            for (int i = 1; i <= 19; i++)
+            {
+                g.DrawLine(p, i * hw, vw, i * hw, pictureBox1.Height - vw);
+                g.DrawLine(p, hw, i * vw, pictureBox1.Width - hw, i * vw);
+            }
+
+            for (int j = 4; j <= 19; j = j + 6)
+            {
+                for (int i = 4; i <= 19; i = i + 6) g.FillEllipse(b, i * hw - 4, j * vw - 4, 8, 8);
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isGame)
+            {
+                Graphics g = pictureBox1.CreateGraphics();
+                int qx = e.X / hw;
+                int rx = e.X % hw;
+                int qy = e.Y / vw;
+                int ry = e.Y % vw;
+
+                if (rx > hw / 2) qx = qx + 1;
+                if (ry > vw / 2) qy = qy + 1;
+
+                if (pan[qx - 1, qy - 1] == 2)
+                {
+                    if (order == 0)
+                    {
+                        g.FillEllipse(black, qx * hw - 8, qy * vw - 8, 16, 16);
+                        pan[qx - 1, qy - 1] = order;
+                        order = 1;
+                    }
+                    else
+                    {
+                        g.FillEllipse(white, qx * hw - 8, qy * vw - 8, 16, 16);
+                        pan[qx - 1, qy - 1] = order;
+                        order = 0;
+                    }
+                }
+                if (checkWinningCondition(qx - 1, qy - 1, order))
+                {
+                    MessageBox.Show("승리하였습니다!");
+                    재개ToolStripMenuItem.Enabled = false;
+                    중지ToolStripMenuItem.Enabled = false;
+                    종료ToolStripMenuItem.Enabled = true;
+                    isGame = false;
+                }
+                g.Dispose();
+            }
+        }
+
+        private void 시작ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            재개ToolStripMenuItem.Enabled = false;
+            중지ToolStripMenuItem.Enabled = true;
+            isGame = true;
+
+
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 19; j++) pan[i,j] = 2;
+            }
+
+            Graphics g = pictureBox1.CreateGraphics();
+            Refresh();
+        }
+
+        private void 중지ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            재개ToolStripMenuItem.Enabled = true;
+            중지ToolStripMenuItem.Enabled = false;
+            isGame = false;
+        }
+         
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void 재개ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            재개ToolStripMenuItem.Enabled = false;
+            중지ToolStripMenuItem.Enabled = true;
+            isGame = true;
+        }
+
+        private bool checkWinningCondition(int x, int y, int o)
+        {
+            int cnt = 1;
+
+            // 오른쪽 방향
+            for (int i = x + 1; i <= 18; i++)
+            {
+                if (pan[i, y] == pan[x, y]) cnt++;
+                else break;
+            }
+            // 왼쪽방향
+            for (int i = x - 1; i >= 0; i--)
+            {
+                if (pan[i, y] == pan[x, y]) cnt++;
+                else break;
+            }
+            if (cnt >= 5) return true;
+
+            cnt = 1;
+
+            // 아래 방향
+            for (int i = y + 1; i <= 18; i++)
+            {
+                if (pan[x, i] == pan[x, y]) cnt++;
+                else break;
+            }
+            // 위 방향
+            for (int i = y - 1; i >= 0; i--)
+            { 
+                if (pan[x, i] == pan[x, y]) cnt++;
+                else break;
+            }
+
+            if (cnt >= 5) return true;
+
+            cnt = 1;
+
+            // 대각선 오른쪽 위방향
+            for (int i = x + 1, j = y - 1; i <= 18 && j >= 0; i++, j--)
+            {
+                if (pan[i, j] == pan[x, y]) cnt++;
+                else break;
+            }
+
+            // 대각선 왼쪽 아래 방향
+            for (int i = x - 1, j = y + 1; i >= 0 && j <= 18; i--, j++)
+            {
+                if (pan[i, j] == pan[x, y]) cnt++;
+                else break;
+            }
+
+            if (cnt >= 5) return true;
+
+            cnt = 1;
+
+            // 대각선 왼쪽 위방향
+            for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (pan[i, j] == pan[x, y]) cnt++;
+                else break;
+            }
+
+            // 대각선 오른쪽 아래 방향 
+            for (int i = x + 1, j = y + 1; i <= 18 && j <= 18; i++, j++)
+            {
+                if (pan[i, j] == pan[x, y]) cnt++;
+                else break;
+            }
+
+            if (cnt >= 5) return true;
+
+            return false;
+        }
+    }
+}
